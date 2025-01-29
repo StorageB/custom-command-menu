2ea02138bdfb6a2f21ae60dcaaecd2dcdb770d49
StorageB <ezzy25@hotmail.com>
2025-01-29 07:46:15 -0600

    update about.js

--- about.js
+++ about.js
@@ -3,14 +3,12 @@
 export const releaseNotes = `
 What's new in this version:
 
-•  Option to use custom text or a custom icon for the menu title instead of the default "Commands" text
-•  Open button added to pop-up notification after exporting commands to directly open/edit the commands.ini file
+•  Support added for GNOME 48.
+•  Fixed a compatibility issue with some systems (such as NixOS) by switching from /bin/bash to /usr/bin/env bash for command execution.
 
 Previous version:
 
-•  Backup and Restore section added to user preferences
-•  Export list of commands to an editable commands.ini file
-•  Import list of commands from the commands.ini file
-•  Support added for GNOME 47
-•  Code revisions to improve efficiency
+•  Option to use custom text or a custom icon for the menu title.
+•  Open button added to pop-up notification after exporting commands to directly open/edit the commands.ini file.
+
 `;
