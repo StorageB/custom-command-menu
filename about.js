96791c1978ffcea19bca2a05e9e907f360f5d6cb
StorageB <ezzy25@hotmail.com>
2025-06-28 16:31:35 -0500

    v10 updates

--- about.js
+++ about.js
@@ -6,9 +6,9 @@ Version 10
 
 What's new in this version:
 
-•  Typing --- or ~~~ in the name field now creates a visual separator line in the menu.
+•  Typing --- or ~~~ in the name field creates a separator line in the menu.
 •  Adding text after --- or ~~~ creates a labeled separator.
-•  An "Insert new" option was added to the row menu (alongside "Duplicate" and "Delete") to allow inserting a new row below an existing one.
+•  An "Insert new" option was added to the row menu (alongside "Duplicate" and "Delete") to insert a new row below an existing one.
 •  A maximize button was added to the preferences window, and the default window size has been changed to better suit a list of commands.
 •  A notification will now appear when the maximum number of command rows is reached.
 •  Fixed a bug where dragging and dropping a row to the same location caused it to jump to the top or bottom of the list.
