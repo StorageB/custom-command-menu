b5eb846e641d2cad40efbfe73e08084aaa3ced00
StorageB <ezzy25@hotmail.com>
2025-09-27 14:55:05 -0500

    updated release notes

--- about.js
+++ about.js
@@ -1,20 +1,13 @@
 /* about.js */
 
 export const releaseNotes = `
-Version 11 - What's new:
+Version 12 - What's new:
 
-•  Option to customize menu location in the top bar.
-•  Support for GNOME 49.
+•  Translations into Italian, Russian, Spanish, French, and German by IlCraccatore2011.
 
 
 Previous version:
 
-•  Typing --- or ~~~ in the name field creates a separator line in the menu.
-•  Adding text after --- or ~~~ creates a labeled separator.
-•  An "Insert new" option was added to the row menu (alongside "Duplicate" and "Delete") to insert a new row below an existing one.
-•  A maximize button was added to the preferences window, and the default window size has been changed to better suit a list of commands.
-•  A notification will now appear when the maximum number of command rows is reached.
-•  Fixed a bug where dragging and dropping a row to the same location caused it to jump to the top or bottom of the list.
-•  Fixed a bug where dragging and dropping or deleting a row sometimes caused the view to scroll to a different location.
-•  Fixed a bug where the row menu sometimes didn't register a selection.
+•  Option to customize menu location in the top bar.
+•  Support for GNOME 49.
 `;
