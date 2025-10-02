5ef65e02a28fb1418e9644772131cefaff5812d0
StorageB <ezzy25@hotmail.com>
2025-10-01 21:45:18 -0500

    updated release notes

--- about.js
+++ about.js
@@ -4,6 +4,7 @@ export const releaseNotes = `
 Version 12 - What's new:
 
 •  Translations into Italian, Russian, Spanish, French, and German by IlCraccatore2011.
+•  Modified the Menu Type setting for improved translation support.
 
 
 Previous version:
