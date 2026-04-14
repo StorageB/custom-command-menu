2c054fc91216f38d3def7fb273461a347494356c
StorageB <ezzy25@hotmail.com>
2026-04-13 22:26:12 -0500

    Update release notes

--- about.js
+++ about.js
@@ -1,7 +1,12 @@
 /* about.js */
 
 export const releaseNotes = `
-Version 13 - What's new:
+Version 14 - What's new:
+
+•  Add dynamic labels using $(command) substitution in names. For example, typing ---$(hostname) displays the hostname as a separator label. Feature contributed by Dylan M. Taylor.
+•  Removed legacy migration code used to upgrade users to the v13 command storage schema (required for the increased number of commands).
+
+Previous version:
 
 •  Submenus! Adding * at the beginning of a command Name creates a submenu entry using the row above as the submenu title.
 •  Added setting to reset all commands and extension settings to defaults.
@@ -9,10 +14,4 @@ Version 13 - What's new:
 •  Improved internal settings handling by significantly reducing the required number of GSettings keys.
 •  Added translations into Japanese, Chinese, Portuguese and Polish by IlCraccatore2011.
 •  Support for GNOME 50.
-
-
-Previous version:
-
-•  Translations into Italian, Russian, Spanish, French, and German by IlCraccatore2011.
-•  Modified the Menu Type setting for improved translation support.
 `;
