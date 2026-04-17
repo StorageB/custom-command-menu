50401f1561be339ea61bb103b205049c7ef91b41
StorageB <ezzy25@hotmail.com>
2026-04-16 21:21:16 -0500

    Update release notes

--- about.js
+++ about.js
@@ -3,7 +3,7 @@
 export const releaseNotes = `
 Version 14 - What's new:
 
-•  Add dynamic labels using $(command) substitution in names. For example, typing ---$(hostname) displays the hostname as a separator label. Feature contributed by Dylan M. Taylor.
+•  Add dynamic labels using $(command) substitution in names! For example, typing ---$(hostname) displays the hostname as a separator label. Feature contributed by Dylan M. Taylor.
 •  Removed legacy migration code used to upgrade users to the v13 command storage schema (required for the increased number of commands).
 
 Previous version:
