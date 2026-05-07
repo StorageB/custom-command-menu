5aefa9bd30e710a042f79ad20b9de86eab35a884
StorageB <ezzy25@hotmail.com>
2026-05-06 22:07:42 -0500

    update release notes

--- about.js
+++ about.js
@@ -1,17 +1,13 @@
 /* about.js */
 
 export const releaseNotes = `
-Version 14 - What's new:
+Version 15 - What's new:
 
-•  Add dynamic labels using $(command) substitution in names! For example, typing ---$(hostname) displays the hostname as a separator label. Feature contributed by Dylan M. Taylor.
-•  Removed legacy migration code used to upgrade users to the v13 command storage schema (required for the increased number of commands).
+•  Use special command ##ShowApplications to open application overview. Feature contributed by Dylan M. Taylor.
+•  Fixed menu icon update and reset issues affecting Bluefin/Dakota.
 
 Previous version:
 
-•  Submenus! Adding * at the beginning of a command Name creates a submenu entry using the row above as the submenu title.
-•  Added setting to reset all commands and extension settings to defaults.
-•  Increased maximum number of entries from 30 to 99.
-•  Improved internal settings handling by significantly reducing the required number of GSettings keys.
-•  Added translations into Japanese, Chinese, Portuguese and Polish by IlCraccatore2011.
-•  Support for GNOME 50.
+•  Add dynamic labels using $(command) substitution in names! For example, typing ---$(hostname) displays the hostname as a separator label. Feature contributed by Dylan M. Taylor.
+•  Removed legacy migration code used to upgrade users to the v13 command storage schema (required for the increased number of commands).
 `;
